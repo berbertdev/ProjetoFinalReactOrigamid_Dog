ae373865aba4f0b5b6772e90c698eccaf488e27c
arthurBerbert <arturberbert@gmail.com>
2023-12-14 15:55:25 -0300

    criado criarLoginRoutes

---
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const LoginPasswordLost = () => {
+  return <div>LoginPasswordLost</div>;
+};
+export default LoginPasswordLost;
---
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const LoginPasswordReset = () => {
+  return <div>LoginPasswordReset</div>;
+};
+export default LoginPasswordReset;
