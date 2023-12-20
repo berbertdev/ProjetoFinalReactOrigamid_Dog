dde30e0ff8076e3e825b2d252cd4ae62e3099743
arthurBerbert <arturberbert@gmail.com>
2023-12-20 10:39:52 -0300

    validateToken adicionado

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Input.module.css';
+
 const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
@@ -11,12 +12,13 @@ const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
         name={name}
         className={styles.input}
         type={type}
-        onChange={onChange}
         value={value}
+        onChange={onChange}
         onBlur={onBlur}
       />
       {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
+
 export default Input;
