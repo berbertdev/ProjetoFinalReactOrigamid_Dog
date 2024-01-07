11020b0014977e829652339f4ddc84576235a80f
arthurBerbert <arturberbert@gmail.com>
2024-01-07 12:50:17 -0300

    UserHeader Adicionado

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import { Navigate, NavLink, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import styles from './UserHeaderNav.module.css';
+
+import MinhasFotos from '../../Assets/feed.svg?react';
+import Estatisticas from '../../Assets/estatisticas.svg?react';
+import Adicionar from '../../Assets/adicionar.svg?react';
+import Sair from '../../Assets/sair.svg?react';
+
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
+  const [mobile, setMobile] = React.useState(null);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    userLogout();
+    navigate('/login');
+  }
+
   return (
-    <nav>
-      <NavLink to="/conta">Minhas Fotos</NavLink>
-      <NavLink to="/conta/estatisticas">Estatisticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar Fotos</NavLink>
-      <button onClick={userLogout}>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && 'Minhas Fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+
+        {mobile && 'Estatisticas'}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <Adicionar />
+
+        {mobile && 'Adicionar Fotos'}
+      </NavLink>
+      <button onClick={handleLogout}>
+        <Sair />
+        {mobile && 'Sair'}
+      </button>
     </nav>
   );
 };
--- src/Components/User/UserHeadre.jsx
+++ src/Components/User/UserHeadre.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import styles from './UserHeadre.module.css';
 import UserHeaderNav from './UserHeaderNav';
+import { useLocation } from 'react-router-dom';
+
 const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+  const { pathname } = location;
+  React.useEffect(() => {
+    switch (pathname) {
+      case '/conta/postar':
+        setTitle('Poste sua foto');
+        break;
+      case '/conta/estatisticas':
+        setTitle('Estatisticas');
+        break;
+      default:
+        setTitle('Minha conta');
+    }
+  }, [location]);
+
   return (
-    <header>
-      <h1 className="title">Titulo</h1>
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
